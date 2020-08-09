923435ce2793e65ca740f7427b427c1746d78966
kamahmad <kam.ahmad17@gmail.com>
2020-08-09 16:06:33 +0530

    Changed the posts feed to include the profile pic and bio(on hover) of the author

--- annoyed-alligators/socl_media/apps/feed/models.py
+++ annoyed-alligators/socl_media/apps/feed/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from ..users.models import Profile
 
 class Post(models.Model):
     """
@@ -12,6 +12,6 @@ class Post(models.Model):
     """
 
     post_content = models.TextField(verbose_name="content")
-    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
     post_date_posted = models.DateTimeField(auto_now_add=True,
                                             verbose_name="Published on")
